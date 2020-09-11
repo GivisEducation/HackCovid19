c896b0437f1695f3f130d9a743de7312402841a1
GivisEducation <giselle.rizo@inacapmail.cl>
2020-09-11 01:34:03 -0300

    continue init

--- src/init.js
+++ src/init.js
@@ -1,3 +1,4 @@
+
 const config = {
     title: "Hi humans",
     scale: {
@@ -17,7 +18,7 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 500 },
+            gravity: { y: 300 },
             debug:false
         }
     }
@@ -27,36 +28,85 @@ const config = {
 var game = new Phaser.Game(config);
 
 function preload() {
-    this.load.setBaseURL('http://labs.phaser.io');
+    //this.load.setBaseURL('http://labs.phaser.io');
 
-    this.load.image('fondo', 'assets/skies/gradient1.png');
-    this.load.image('particula', 'assets/particles/yellow.png');
+    //this.load.image('fondo', 'assets/skies/gradient1.png');
+    //this.load.image('particula', 'assets/particles/yellow.png');
+
+    this.load.spritesheet('Humano', 'Humano.png', { frameWidth: 32.5, frameHeight: 48 });
+
 };
 function create() {
+    this.load.setPath('./Assets/');
+    this.load.image([
+        'water',
+        'lucky',
+        'Plataforma',
+        'fondo2',
+    ]);
+    this.add.image(400, 300, 'fondo2').setScale(1, 1.15);
+
+
     console.log(this.add.image(400, 300, 'fondo'));
-    particulas = this.add.particles('particula');
-    var emitter = particulas.createEmitter({
-        speed: 50,
-        scale: { start: 2, end: 0 },
-        blendMode: 'ADD'
-    });
+    Plataforma = this.physics.add.staticGroup();
+    Plataforma.create(400, 590, 'Plataforma').setScale(2.1, 1).refreshBody();
+    Plataforma.create(400, 0, 'Plataforma').setScale(2.1, 1).refreshBody();
+    Plataforma.create(700, 410, 'Plataforma').setScale(0.3, 1).refreshBody();
+    Plataforma.create(400, 300, 'Plataforma').setScale(0.2, 1).refreshBody();
+    Plataforma.create(800, 150, 'Plataforma');
+    Plataforma.create(-50, 300, 'Plataforma');
+    Plataforma.create(0, 450, 'Plataforma');
+    Plataforma.getChildren()[0].setOffset(0, 10);
+    Humano = this.physics.add.sprite(230, 100, 'Humano');
+    Humano.setCollideWorldBounds(true);
+    Humano.setBounce(0.2);
+    this.physics.add.collider(Humano, Plataforma);
+
+    //particulas = this.add.particles('particula');
+    //var emitter = particulas.createEmitter({
+    //    speed: 50,
+    //    scale: { start: 2, end: 0 },
+    //    blendMode: 'ADD'
+    //});
    
-    this.add.text(300, 500, 'Hi humans');
+    this.add.text(300, 500, 'Please');
+    this.anims.create({
+        key: 'Izquierda',
+        frames: this.anims.generateFrameNumbers('Humano', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'Derecha',
+        frames: this.anims.generateFrameNumbers('Humano', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'Quieto',
+        frames: [{ key: 'Humano', frame: 4 }],
+        frameRate: 20
+    });
 
 };
 function update(time, delta) {
-    //if (keyD.isDown) {
-    //    console.log('La tecla W está presionada');
-    //} else if (keyD.isUp)
-    //    console.log('La tecla W , no está presionada')
+    cursors = this.input.keyboard.createCursorKeys();
+    if (cursors.left.isDown) {
+        Humano.setVelocityX(-200);
+        Humano.anims.play('Izquierda', true);
+    }
+    else if (cursors.right.isDown) {
+        Humano.setVelocityX(200);
+        Humano.anims.play('Derecha', true);
+    }
+    else {
+        Humano.setVelocityX(0);
+        Humano.anims.play('Quieto');
+    }
 
-
-    //const eventos = Phaser.Input.Events;
-    //console.log(eventos);
-    //const KeyCodes = Phaser.Input.Keyboard.keycodes;
-    //console.log(KeyCodes);
-    //keyD = this.input.keyboard.addKey(KeyCodes.W);
-    //keyD.on('down', () => {
-    //    console.log('soy la letra W');
-    //});
+    if (cursors.up.isDown && Humano.body.touching.down) {
+        Humano.setVelocityY(-310);
+    }
 };
