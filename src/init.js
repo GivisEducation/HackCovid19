fdd292936294423441a98ad22ea24eea142ab334
GivisEducation <giselle.rizo@inacapmail.cl>
2020-09-11 10:24:55 -0300

    Cambio el init

--- src/init.js
+++ src/init.js
@@ -1,15 +1,13 @@
-
 const config = {
-    title: "Hi humans",
+    title: "HackCovid19",
     scale: {
-        mode: Phaser.Scale.Fit,
+        mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
         type: Phaser.AUTO,
         parent: "contenedor",
         width: 800,
         height: 600,
     },
-
     scene: {
         preload,
         create,
@@ -19,94 +17,142 @@ const config = {
         default: 'arcade',
         arcade: {
             gravity: { y: 300 },
-            debug:false
+            debug: false
         }
-    }
+    },
 
-};
 
+}
+var gameOver = false;
+var Puntos = 0;
 var game = new Phaser.Game(config);
 
+
 function preload() {
-    //this.load.setBaseURL('http://labs.phaser.io');
-
-    //this.load.image('fondo', 'assets/skies/gradient1.png');
-    //this.load.image('particula', 'assets/particles/yellow.png');
-
-    this.load.spritesheet('Humano', 'Humano.png', { frameWidth: 32.5, frameHeight: 48 });
-
-};
-function create() {
     this.load.setPath('./Assets/');
     this.load.image([
-        'water',
-        'lucky',
+        'Lucky',
+        'Water',
+        'Fondo',
         'Plataforma',
-        'fondo2',
     ]);
-    this.add.image(400, 300, 'fondo2').setScale(1, 1.15);
-
-
-    console.log(this.add.image(400, 300, 'fondo'));
-    Plataforma = this.physics.add.staticGroup();
-    Plataforma.create(400, 590, 'Plataforma').setScale(2.1, 1).refreshBody();
-    Plataforma.create(400, 0, 'Plataforma').setScale(2.1, 1).refreshBody();
-    Plataforma.create(700, 410, 'Plataforma').setScale(0.3, 1).refreshBody();
-    Plataforma.create(400, 300, 'Plataforma').setScale(0.2, 1).refreshBody();
-    Plataforma.create(800, 150, 'Plataforma');
-    Plataforma.create(-50, 300, 'Plataforma');
-    Plataforma.create(0, 450, 'Plataforma');
-    Plataforma.getChildren()[0].setOffset(0, 10);
-    Humano = this.physics.add.sprite(230, 100, 'Humano');
-    Humano.setCollideWorldBounds(true);
-    Humano.setBounce(0.2);
-    this.physics.add.collider(Humano, Plataforma);
-
-    //particulas = this.add.particles('particula');
-    //var emitter = particulas.createEmitter({
-    //    speed: 50,
-    //    scale: { start: 2, end: 0 },
-    //    blendMode: 'ADD'
-    //});
-   
-    this.add.text(300, 500, 'Please');
+    this.load.spritesheet('Kaze', 'Kaze.png', { frameWidth: 32.5, frameHeight: 48 });
+    plataforma = this.physics.add.staticGroup();
+};
+function create() {
+    this.add.image(400, 300, 'Fondo').setScale(1, 1.15);
+    plataforma.create(400, 590, 'Plataforma').setScale(2.1, 1).refreshBody();
+    plataforma.create(400, 0, 'Plataforma').setScale(2.1, 1).refreshBody();
+    plataforma.create(700, 410, 'Plataforma').setScale(0.3, 1).refreshBody();
+    plataforma.create(400, 300, 'Plataforma').setScale(0.2, 1).refreshBody();
+    plataforma.create(800, 150, 'Plataforma');
+    plataforma.create(-50, 300, 'plataforma');
+    plataforma.create(0, 450, 'Plataforma');
+    plataforma.getChildren()[0].setOffset(0, 10);
+    Kaze = this.physics.add.sprite(230, 100, 'Kaze');
+    Kaze.setCollideWorldBounds(true);
+    Kaze.setBounce(0.2);
     this.anims.create({
         key: 'Izquierda',
-        frames: this.anims.generateFrameNumbers('Humano', { start: 0, end: 3 }),
+        frames: this.anims.generateFrameNumbers('Kaze', { start: 0, end: 3 }),
         frameRate: 10,
         repeat: -1
     });
 
     this.anims.create({
         key: 'Derecha',
-        frames: this.anims.generateFrameNumbers('Humano', { start: 5, end: 8 }),
+        frames: this.anims.generateFrameNumbers('Kaze', { start: 5, end: 8 }),
         frameRate: 10,
         repeat: -1
     });
 
     this.anims.create({
         key: 'Quieto',
-        frames: [{ key: 'Humano', frame: 4 }],
+        frames: [{ key: 'Kaze', frame: 4 }],
         frameRate: 20
     });
 
-};
-function update(time, delta) {
+    this.physics.add.collider(Kaze, plataforma);
+    plataforma.getChildren()[0].setOffset(0, 10);
     cursors = this.input.keyboard.createCursorKeys();
-    if (cursors.left.isDown) {
-        Humano.setVelocityX(-200);
-        Humano.anims.play('Izquierda', true);
-    }
-    else if (cursors.right.isDown) {
-        Humano.setVelocityX(200);
-        Humano.anims.play('Derecha', true);
-    }
-    else {
-        Humano.setVelocityX(0);
-        Humano.anims.play('Quieto');
+
+    plantita = this.physics.add.group({
+        key: 'Lucky',
+        repeat: 5,
+        setXY: { x: 12, y: 50, stepX: 140 }
+    });
+    plantita.children.iterate(function (child) {
+        child.setBounce(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
+
+    this.physics.add.collider(plataforma, plantita);
+
+    this.physics.add.overlap(Kaze, plantita, esconder, null, this);
+
+    PuntosTexto = this.add.text(300, 560, 'Puntos: 0', { fontSize: '40px', fill: 'red' });
+    enemigos = this.physics.add.group();
+    this.physics.add.collider(enemigos, plataforma);
+    this.physics.add.collider(Kaze, enemigos, choque, null, this);
+
+    function esconder(Kaze, plantita) {
+
+        plantita.disableBody(true, true);
+
+        Puntos += 10;
+        PuntosTexto.setText('Puntos:' + Puntos);
+        if (plantita.countActive(true) === 0) {
+
+            plantita.children.iterate(function (child) {
+
+                child.enableBody(true, child.x, 50, true, true)
+
+            });
+
+            var x = (Kaze.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+            Water = enemigos.create(x, 16, 'Water');
+            Water.setBounce(1);
+            Water.setCollideWorldBounds(true);
+            Water.setVelocity(Phaser.Math.Between(-100, 100), 5);
+
+        }
+
+
     }
 
-    if (cursors.up.isDown && Humano.body.touching.down) {
-        Humano.setVelocityY(-310);
+    function choque(Kaze, Water) {
+        this.physics.pause();
+        Kaze.anims.play('Quieto');
+        gameOver = true;
     }
+
+
+
+
 };
+function update(time, delta) {
+    if (gameOver) {
+        return;
+    }
+
+
+    if (cursors.left.isDown) {
+        Kaze.setVelocityX(-200);
+        Kaze.anims.play('Izquierda', true);
+    }
+    else if (cursors.right.isDown) {
+        Kaze.setVelocityX(200);
+        Kaze.anims.play('Derecha', true);
+    }
+    else {
+        Kaze.setVelocityX(0);
+        Kaze.anims.play('Quieto');
+    }
+
+    if (cursors.up.isDown && Kaze.body.touching.down) {
+        Kaze.setVelocityY(-310);
+    }
+
+
+
+}
